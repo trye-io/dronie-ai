2131659312637f894ed902ef4818468857db0f6d
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2023-12-14 16:38:33 +0100

    Add dirty files stream and process video to recognize gestures with new version of mediapipe. Ignore pycashe

--- level-1/0-cv.py
+++ level-1/0-cv.py
@@ -3,7 +3,7 @@ from djitellopy import Tello
 import numpy as np 
 import cv2 
 import mediapipe as mp
-# from visualize_hand_landmark_detection import draw_landmarks_on_image
+from visualize_hand_landmark_detection import draw_landmarks_on_image
 
 # ---
 # Loading and setting up mediapipe
@@ -18,21 +18,12 @@ GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 def render_frame(result, output_image, timestamp_ms):
-    print("1")
 
-    global is_flying
+    if result.gestures:
+        for gesture in result.gestures:
+            print(gesture[0].category_name)
 
-    frame = output_image.numpy_view()
-
-    # frame = draw_landmarks_on_image(output_image.numpy_view(), result)
-
-    # if result.gestures:
-    #     for gesture in result.gestures:
-    #         print(gesture[0].category_name)
-    #         if gesture[0].category_name == "Thumb_Up" and not(is_flying):
-    #             drone.takeoff()
-    #         if gesture[0].category_name == "Thumb_Down" and is_flying:
-    #             drone.land()
+    frame = draw_landmarks_on_image(output_image.numpy_view(), result)
 
     frame = cv2.resize(frame, (WIDTH, HEIGHT)) 
     frame = np.rot90(frame)
@@ -55,7 +46,6 @@ FPS = 30
 clock = pygame.time.Clock() 
 
 is_running = True
-is_flying = False
 
 drone = Tello()
 drone.connect()
@@ -66,34 +56,19 @@ with GestureRecognizer.create_from_options(options) as recognizer:
     while is_running: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-
-                if is_flying: 
-                    drone.land()
-                    is_flying = False
-
                 drone.streamoff()
-            
                 is_running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_t and not(is_flying):
-                    drone.takeoff()
-                    is_flying = True
-                if event.key == pygame.K_l and is_flying:
-                    drone.land()
-                    is_flying = False
 
-    frame = frame_read.frame 
+        frame = frame_read.frame 
 
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    timestamp += 1
+        timestamp += 1
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
 
-    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-
-    recognizer.recognize_async(
-        mp_image,
-        timestamp # see https://github.com/google/mediapipe/issues/4448
-    )
-        
-    pygame.display.flip() 
-    clock.tick(FPS)
+        recognizer.recognize_async(
+            mp_image,
+            timestamp # see https://github.com/google/mediapipe/issues/4448
+        )
+        pygame.display.flip() 
+        clock.tick(FPS)
---
+++ level-1/visualize_hand_landmark_detection.py
@@ -0,0 +1,47 @@
+# Taken from https://colab.research.google.com/github/googlesamples/mediapipe/blob/main/examples/hand_landmarker/python/hand_landmarker.ipynb#scrollTo=_JVO3rvPD4RN&line=11&uniqifier=1
+
+import cv2
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import numpy as np
+
+MARGIN = 10  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+  hand_landmarks_list = detection_result.hand_landmarks
+  handedness_list = detection_result.handedness
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected hands to visualize.
+  for idx in range(len(hand_landmarks_list)):
+    hand_landmarks = hand_landmarks_list[idx]
+    handedness = handedness_list[idx]
+
+    # Draw the hand landmarks.
+    hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    hand_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+    ])
+    solutions.drawing_utils.draw_landmarks(
+      annotated_image,
+      hand_landmarks_proto,
+      solutions.hands.HAND_CONNECTIONS,
+      solutions.drawing_styles.get_default_hand_landmarks_style(),
+      solutions.drawing_styles.get_default_hand_connections_style())
+
+    # Get the top left corner of the detected hand's bounding box.
+    height, width, _ = annotated_image.shape
+    x_coordinates = [landmark.x for landmark in hand_landmarks]
+    y_coordinates = [landmark.y for landmark in hand_landmarks]
+    text_x = int(min(x_coordinates) * width)
+    text_y = int(min(y_coordinates) * height) - MARGIN
+
+    # Draw handedness (left or right hand) on the image.
+    cv2.putText(annotated_image, f"{handedness[0].category_name}",
+                (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
+                FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
+
+  return annotated_image
