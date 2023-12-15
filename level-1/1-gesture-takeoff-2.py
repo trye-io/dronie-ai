2932b3b5d257c4d7eb8ff035981c81be3a0ebd63
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2023-12-15 14:33:10 +0100

    Add threading for taking off and landing

--- level-1/1-gesture-takeoff-2.py
+++ level-1/1-gesture-takeoff-2.py
@@ -3,9 +3,9 @@ from djitellopy import Tello
 import numpy as np 
 import cv2 
 import mediapipe as mp
+import threading
 from visualize_hand_landmark_detection import draw_landmarks_on_image
 
-# Loading and setting up mediapipe
 MODEL_PATH = 'level-1/gesture_recognizer.task'
 WIDTH = 960 # 384
 HEIGHT = 720 # 288
@@ -18,9 +18,7 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 
 def render_frame(result, output_image, timestamp_ms):
 
-    if result.gestures:
-        for gesture in result.gestures:
-            print(gesture[0].category_name)
+    global is_flying
 
     frame = draw_landmarks_on_image(output_image.numpy_view(), result)
 
@@ -29,6 +27,15 @@ def render_frame(result, output_image, timestamp_ms):
     frame = np.flipud(frame) 
     frame = pygame.surfarray.make_surface(frame)
     screen.blit(frame, (0,0))
+
+    if result.gestures:
+        for gesture in result.gestures:
+            if gesture[0].category_name == "Thumb_Up" and not(is_flying):
+                threading.Thread(target=drone.takeoff).start()
+                is_flying = True
+            if gesture[0].category_name == "Thumb_Down" and is_flying:
+                threading.Thread(target=drone.land).start()
+                is_flying = False
 
 options = GestureRecognizerOptions(
     base_options=BaseOptions(model_asset_path=MODEL_PATH),
@@ -45,6 +52,7 @@ drone = Tello()
 drone.connect()
 drone.streamon()
 frame_read = drone.get_frame_read() 
+is_flying = False
 
 timestamp = 0
 is_running = True
@@ -53,8 +61,18 @@ with GestureRecognizer.create_from_options(options) as recognizer:
     while is_running: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                if is_flying: 
+                    is_flying = False
+                    drone.land()
                 drone.streamoff()
                 is_running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_t and not(is_flying):
+                    is_flying = True
+                    drone.takeoff()
+                if event.key == pygame.K_l and is_flying:
+                    is_flying = False
+                    drone.land()
 
         frame = frame_read.frame 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
