6a6ad97fa68b8450b29bf22649e3c39a9eabc317
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2024-02-11 20:04:06 +0100

    Refactor slightly and comment

--- level-3/2-manual.py
+++ level-3/2-manual.py
@@ -3,26 +3,24 @@ from djitellopy import Tello
 import numpy as np 
 import cv2 
 from ultralytics import YOLO
-import numpy # завантажуємо numpy для типу змінної
 import threading # завантажуємо threding для потокового виконання
 
 WIDTH = 640
 HEIGHT = 480
-# TARGET = (WIDTH / 2, 135) # координату висоти взяли з власного пролітання через рамку
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-FPS = 5 # Нижча кількість FPS 
+FPS = 5
 clock = pygame.time.Clock() 
 
-MODEL_PATH = "level-3/box-detector.pt"
-model = YOLO(MODEL_PATH)
+model = YOLO("level-3/box-detector.pt")
 
 drone = Tello()
 drone.connect()
 drone.streamon()
 frame_read = drone.get_frame_read()
 
+# швидкості дрона
 left_right_velocity = 0
 forward_backward_velocity = 0
 up_down_velocity = 0
@@ -30,34 +28,32 @@ yaw_velocity = 0
 
 is_running = True
 
-status = 0
-GROUNDED = 0
-MANUAL = 1 # manual mode
-TRACKING = 2 # tracking mode
-
-# Kp = (0.125, 0.125)
+GROUNDED = 0 # режим на землі
+MANUAL = 1 # режим ручного керування
+TRACKING = 2 # режим автопілота
+mode = GROUNDED
 
 while is_running: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            if status != GROUNDED: 
+            if mode != GROUNDED: 
                 threading.Thread(target=drone.land).start()
-                status = GROUNDED
+                mode = GROUNDED
             drone.streamoff()
             is_running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_0 and status != GROUNDED:
+            if event.key == pygame.K_0 and mode != GROUNDED:
                 # drone.send_rc_control(0, 0, 0, 0)
                 threading.Thread(target=drone.land).start()
-                status = GROUNDED
+                mode = GROUNDED
             if event.key == pygame.K_1:
-                if status == GROUNDED:
+                if mode == GROUNDED:
                     threading.Thread(target=drone.takeoff).start()
-                if status == TRACKING:
+                if mode == TRACKING:
                     drone.send_rc_control(0, 0, 0, 0)
-                status = MANUAL
-            if event.key == pygame.K_2 and status == MANUAL:
-                status = TRACKING
+                mode = MANUAL
+            if event.key == pygame.K_2 and mode == MANUAL:
+                mode = TRACKING
 
             if event.key == pygame.K_LEFT:
                 left_right_velocity = 50
@@ -94,17 +90,17 @@ while is_running:
                 yaw_velocity = 0
 
     frame = frame_read.frame 
-    frame = cv2.resize(frame, (WIDTH, HEIGHT)) # зменшуємо розмір зображення
+    frame = cv2.resize(frame, (WIDTH, HEIGHT))
 
     results = model.predict(frame)
 
     for bbox in results[0].boxes:
-        xyxy = bbox.numpy().xyxy.astype(numpy.int64).flatten()
+        xyxy = bbox.numpy().xyxy.astype(np.int8).flatten()
         cv2.rectangle(
             frame,
             (xyxy[0], xyxy[1]),
             (xyxy[2], xyxy[3]),
-            color=(255, 0, 0),
+            color=(0, 0, 255),
             thickness=2
         )
 
@@ -115,14 +111,14 @@ while is_running:
     screen.blit(frame, (0, 0))
 
     
-    if status == MANUAL:
+    if mode == MANUAL:
         drone.send_rc_control(
             left_right_velocity,
             forward_backward_velocity,
             up_down_velocity,
             yaw_velocity
         )
-    if status == TRACKING:
+    if mode == TRACKING:
         print("tracking")
         
     pygame.display.flip() 
